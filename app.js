34ccafdea8f4cf1f7811b2bea41494b263a5f22b
z-kalanaki <z-kalanaki@agri-bank.com>
2022-09-26 15:05:02 +0330

    Add your commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const container = document.getElementById("create");
+
 class LinkedList {
   constructor() {
     this.first = null;
@@ -32,15 +34,148 @@ class LinkedList {
     return elementsList;
   }
 
-  draw(){
-    alert("draw")
+  draw() {
+    document.querySelectorAll(".node").forEach((note) => note.remove());
+
+    const mlist = this.toArray();
+
+    if (mlist.length == 0) {
+      container.style.display = "block";
+      return;
+    }
+
+    container.style.display = "none";
+
+    if (mlist.length == 1) {
+
+      container.insertAdjacentHTML(
+        "afterend",
+        `<div id="node">
+            <div class="node-header">
+                <div class="indicator">
+                    <p>Head & Tail</p>
+                    <span class="material-symbols-rounded size-48">upgrade</span>
+                </div>
+            </div>
+            <div class="indicate">
+                <div class="node-body ">
+                    <div class="value">
+                        <p>first</p>
+                        
+                    </div>
+                    <div class="next">
+                        <span class="material-symbols-rounded  size-48">cancel</span>
+                    </div>
+                </div>
+                <div class="modify">
+                    <span class="material-symbols-rounded">delete</span>
+                    <span class="material-symbols-rounded">add_box</span>
+                </div>
+            </div>
+        </div>`
+      );
+      return;
+    }
+
+    container.style.display = "none";
+
+    mlist.forEach((note, index) => {
+      if (index == 0) {
+        container.insertAdjacentHTML(
+          "afterend",
+          `<div class="node">
+                <div class="node-header">
+                    <div class="indicator">
+                        <p>Head</p>
+                        <span class="material-symbols-rounded size-48">upgrade</span>
+                    </div>
+                </div>
+                <div class="indicate">
+                    <div class="node-body ">
+                        <div class="value">
+                            <p>first</p>
+                            
+                        </div>
+                        <div class="next">
+                            <span  class="material-symbols-rounded size-48">radio_button_checked</span>
+                            <div class="next-arrow-container">                
+                                <span class="material-symbols-rounded  size-60">arrow_right_alt</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modify">
+                        <span class="material-symbols-rounded">delete</span>
+                        <span class="material-symbols-rounded">add_box</span>
+                    </div>
+                </div>
+            </div>`
+        );
+      }
+
+ if(index == (mlist.length - 1) ){
+  container.insertAdjacentHTML(
+    "afterend",`
+        <div class="node">
+            <div class="node-header">
+                <p>Tail</p>
+                <span class="material-symbols-rounded size-48 ">upgrade</span>
+               
+            </div>
+            <div class="indicate">
+                <div class="node-body ">
+                    <div class="value">
+                        <p>last</p>
+                        
+                    </div>
+                    <div class="next">
+                        <span class="material-symbols-rounded  size-48">cancel</span>
+                    </div>
+                </div>
+                <div class="modify">
+                    <span class="material-symbols-rounded">delete</span>
+                    <span class="material-symbols-rounded">add_box</span>
+                </div>
+            </div>
+           
+        </div>`)
+ }
+
+ if (index != 0 && index != (mlist.length - 1)) {
+  container.insertAdjacentHTML(
+    "afterend",`
+    <div class="node">
+    <div class="node-header">
+    </div>
+    <div class="indicate">
+        <div class="node-body ">
+            <div class="value">
+                <p>middle</p>
+                
+            </div>
+            <div class="next">
+            <span  class="material-symbols-rounded size-48">radio_button_checked</span>
+            <div class="next-arrow-container">                
+                <span class="material-symbols-rounded  size-60">arrow_right_alt</span>
+            </div>
+            </div>
+        </div>
+        <div class="modify">
+            <span class="material-symbols-rounded">delete</span>
+            <span class="material-symbols-rounded">add_box</span>
+        </div>
+    </div>
+   
+</div>`)
+ }
+
+    });
   }
 }
 
 const linkedList = new LinkedList();
 linkedList.append(1);
 linkedList.append("1");
-linkedList.append(true);
+ linkedList.append(true);
 linkedList.draw();
 
 console.log(linkedList.toArray());
